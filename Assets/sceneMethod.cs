40d03ac86e013e14ed116d7d2bfc4cfbd5542ad5
Krayden <kenntheus24@gmail.com>
2023-10-02 21:32:18 +0800

    login password inputfield bugged nakakaputangina

--- Assets/sceneMethod.cs
+++ Assets/sceneMethod.cs
@@ -6,25 +6,25 @@ using UnityEngine.SceneManagement;
 public class sceneMethod : MonoBehaviour
 {
     public void loadCustomizeScene(){
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(4);
     }
 
     public void loadProfileScene(){
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(5);
     }
 
 
     public void loadShopScene(){
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(6);
     }
 
     public void loadSettingsScene(){
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(7);
     }
 
     public void backToLobby(){
         Debug.Log("BackToLobby called");
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(3);
     }
 
     public void callMethod(){
