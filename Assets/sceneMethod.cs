6095d168cb298da1ceab6dcddc9059492c0d35cd
Yurei <glba.alicoben.up@phinmaed.com>
2023-10-06 21:27:46 +0800

    ano to

---
+++ Assets/sceneMethod.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneMethod : MonoBehaviour
+{
+    public void loadCustomizeScene(){
+        SceneManager.LoadScene(4);
+    }
+
+    public void loadProfileScene(){
+        SceneManager.LoadScene(5);
+    }
+
+
+    public void loadShopScene(){
+        SceneManager.LoadScene(6);
+    }
+
+    public void loadSettingsScene(){
+        SceneManager.LoadScene(7);
+    }
+
+    public void backToLobby(){
+        Debug.Log("BackToLobby called");
+        SceneManager.LoadScene(3);
+    }
+
+    public void callMethod(){
+        Debug.Log("Mode 2 opened");
+    }
+
+}
